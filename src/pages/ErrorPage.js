c9269e8a5a9fa8ab6803a12d15dcade56f2be178
Bruno Rossi <brunogabrielrossi@gmail.com>
2024-01-05 10:15:00 -0500

    Added simple Error page

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ErrorPage() {
+      
+    return (
+        
+        <div>
+            <h1>Oops, looks like there was an error!</h1>
+        </div>
+    )
+}
+
+export default ErrorPage;
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import App from "./App";
 import Parks from "./pages/Parks";
 import Passport from "./pages/Passport";
 import ParkPage from "./pages/ParkPage";
+import ErrorPage from "./pages/ErrorPage";
 
 const routes = [
     {
         path: '/',
         element: <App />,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: '/',
